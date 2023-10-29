2ef03bac91185fa0cd2276c2b8211e1696068bdf
logunagini <logeshwari515logu@gmail.com>
2023-10-29 12:02:39 +0530

    jsmovies

--- helo.js
+++ helo.js
@@ -119,7 +119,7 @@ console.log(nithiya,vaishu,average);
 let l,k;
 l=k=25 - 5 -10;
 console.log(l,k);
-*/
+
 let mark_mass = 95;
 let john_mass = 85;
 let mark_height= 1.88;
@@ -131,4 +131,60 @@ let markHightBMI = (bmi_mark > bmi_john);
 
 console.log(bmi_mark,bmi_john);
 console.log(markHightBMI);
+console.log('');
 
+console.log('');*/
+/*const firstName = "logeshwari";
+const present = "student";
+const birthYear = 2006;
+const year = 2023;
+
+const logi = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + present + '!';
+console.log(logi);
+
+const logiNew = "I' m ${firstName}, a ${year - birthYear}  years old ${present}!";
+console.log(logiNew);
+
+console.log("string with /n/ multiple /n/ lines");*/
+let favorite = prompt('who are you movie');
+let movie1 = 'leo';
+let movie2 = 'vikram';
+let movie3 = 'kaithi';
+let movie4 ='rrr';
+
+// if("leo" in lcu ){
+//     console.log("true")
+// }
+// else{
+//     console.log("False")
+// };
+
+const lcu = ['leo','vikram','rrr'];
+
+// console.log(0 in lcu);
+
+// const number = [2, 3, 4, 5];
+
+// console.log(3 in number);
+
+//lcu = ['leo','vikram','rrr']
+ 
+if (lcu.includes(favorite)) {
+    console.log('lokesh movie1 director')
+} else if (lcu.includes(favorite)) {
+    console.log('lokesh movie2 director')
+} else if (lcu.includes(favorite)) {
+    console.log('lokesh movie3 director')
+}  else if (lcu.includes(favorite))  {
+    console.log('lokesh movie4 director')
+} else {
+    console.log('lokesh not movies director')
+}
+
+
+//if(lcu.includes(favorite)){
+    //console.log("True")
+//}
+//else{
+    //console.log("False")
+//}
--- hi.js
+++ hi.js
@@ -1,6 +1,18 @@
-functionSquare(logu)
-{
-    let x=5;
-    x=x+logu
-    return x
-} 
+
+let favorite = prompt('who are you movie');
+let movie1 = 'leo';
+let movie2 = 'vikram';
+let movie3 = 'kaithi';
+let movie4 ='rrr';
+const lcu = ['leo','vikram','rrr'];
+if (lcu.includes(favorite)) {
+    console.log('lokesh movie director')
+} else if (lcu.includes(favorite)) {
+    console.log('lokesh movie director')
+} else if (lcu.includes(favorite)) {
+    console.log('lokesh movie director')
+}  else if (lcu.includes(favorite))  {
+    console.log('lokesh movie director')
+} else {
+    console.log('lokesh not movies director')
+}
