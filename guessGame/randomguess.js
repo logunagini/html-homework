61b543d85408dedef40f6ac8d1869054fbe85a13
logunagini <logeshwari515logu@gmail.com>
2024-02-12 17:25:52 +0530

    jsfirstgame _changes

--- guessGame/randomguess.js
+++ guessGame/randomguess.js
@@ -1,7 +1,7 @@
 'use strict';
  
  let randomNumber = Math.trunc(Math.random() * 20) +1;
- console.log(randomNumber);  
+  
  let score = 20
  let highscore = 0
  const displayChange = function(message){
@@ -10,21 +10,24 @@
  const displayScore = function(score){
     document.querySelector('.score').textContent = score
  };
-    document.querySelector('.check').addEventListener('click', function () {
+    document.querySelector('.check').addEventListener('click', function()
+    {
     const guessNumber = Number(document.querySelector('.guess').value);
-    console.log(guessNumber);
+   
    
 if (!guessNumber) {
     displayChange("enter number!';😫😪😫😫😪🤐")
     } else if (randomNumber === guessNumber) {
-        displayChange('Correct Number!😁😀😁😀😁😀👏👏👏 ');
+        displayChange('Correct Number!😁😀😁😀😁😀👏👏👏 '); 
     document.querySelector('.number').textContent = randomNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     if (score > highscore){
          highscore = score
         document.querySelector('.highscore').textContent = highscore 
-    }
+    } 
+        
+    
 } else if (randomNumber !== guessNumber){
         if (score >= 1) {
             displayChange((randomNumber < guessNumber) ?
@@ -34,20 +37,30 @@ if (!guessNumber) {
         }else {
             displayChange("you lost the game😵‍💫😵😵‍💫😵😵‍💫😵");
             displayScore(0);
+            
+            document.querySelector('.number').textContent = randomNumber;
             document.querySelector('body').style.backgroundColor = 'red'; 
     }
+
     } 
-        }
-        );
- // running the old time new page 
-        document.querySelector('.again').addEventListener('click', function () {
-        let score = 20
-        let randomNumber = Math.trunc(Math.random() * 20) +1;
-        displayChange("Start guessing...");
+        });
         
+ // running the old time new page again button
+        document.querySelector('.again').addEventListener('click', function () {
+        let score = 20;
+
+        displayChange("Start guessing...");
+    
         displayScore(score)
+        
         document.querySelector('body').style.backgroundColor = '#222';
         document.querySelector('.number').textContent = '?';
         document.querySelector('.guess').value = "";
         document.querySelector('.number').style.width = '15rem';
+        
+        randomNumber = Math.trunc(Math.random() * 20) +1;
+        
+        //let randomNumber = Math.trunc(Math.random() * 20) +1;
+        //console.log(randomNumber);
+        //console.log(Check);
         });  
