bd3780d7742e701a89e9f0accb685239d4549376
logunagini <logeshwari515logu@gmail.com>
2024-02-26 22:31:26 +0530

    sivaanna

--- pigGame/big_game.js
+++ pigGame/big_game.js
@@ -2,19 +2,23 @@
 // game play panna instruction solla
 // html connenting
 const modal = document.querySelector(".modal");
+// const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnOpenModal = document.querySelectorAll(".run");
+
 // gamela start button click panna instruction kattum
+
 const openModal = function () {
     modal.classList.remove('hidden');
-};
-for (let x = 0; x < btnOpenModal.length; x++)
-{
+    // overlay.classList.remove('hidden');
+}; 
+for (let x = 0; x < btnOpenModal.length; x++){
     btnOpenModal[x].addEventListener('click',openModal)
  };
 // gamela instruction button na pannum
 const closeModal = function () {
     modal.classList.add('hidden');
+    // overlay.classList.add('hidden');
    
     //document.querySelector('body').style.backgroundColor = 'red'; 
 };
@@ -23,7 +27,11 @@ btnCloseModal.addEventListener('click',closeModal);
 
 
 
+
 // connecting HTML
+
+// let input2 = document.getElementById("player2")
+
 let Name1 = document.getElementById("name--0")
 let Name2 = document.getElementById("name--1")
 // current 1 & 2 current boxla value increment panna
@@ -43,6 +51,12 @@ let btnHold = document.querySelector(".btn--hold")
 // intha yaru winnernu solla player1 & 2
 let player1 = document.querySelector("player--0")
 let player2 = document.querySelector("player--1")
+// let checkingBtn=document.getElementById("check");
+
+//  function update () {
+//   input1 = input1.textContent
+
+// }
 
 let cur1Score = 0; 
 let cur2Score = 0; 
@@ -65,7 +79,7 @@ btnRollDice.addEventListener('click', function () {
         //diceRoller == 1 condition false player 2 call pannum
         if (diceRoller == 1) {
             cur1Score = 0;
-            document.querySelector('.player--0').classList.remove('player--active')
+            document.querySelector('.player--0').classList.toggle('player--active')
             Current1.textContent = cur1Score;
             CurrentPlayer = Current2; 
             document.querySelector('.player--1').classList.toggle('player--active')
@@ -97,14 +111,14 @@ btnRollDice.addEventListener('click', function () {
     }
 );
 // btnHold button click panna player 1 & 2 la yaru winnernu kattum and colorla change pannum
-
+//(player1Score >= 10 && player2Score < 10) 
 btnHold.addEventListener('click', function () {
  //if condition ture player 1 winner
-    if (player1Score >= 10 && player2Score < 10) {
+    if (player1Score >= 10) {
         document.querySelector('.player--0').classList.add('player--winner')
         Name1.textContent = " Player-1 is a Winner congratulations !..👏👏👏👏 ";
        // if condition false na player 2 winner
-    } else if (player2Score >= 10 && player1Score < 10) {
+    } else if (player2Score >= 10) {
         document.querySelector('.player--1').classList.add('player--winner')
         Name2.textContent = " Player-2 is a Winner congratulations !..👏👏👏👏 ";
     }
